48f7acb6fbe49d0c7b17d33f670b881d86fd27fe
ssemenkoff <ssemenkoff@gmail.com>
2025-09-08 12:27:41 +0200

    Created ogc features composer

---
+++ packages/ui/vue/composer/ogc/src/Settings.vue
@@ -0,0 +1,60 @@
+<!--
+Copyright (c) 2025 Contributors to the Eclipse Foundation.
+
+This program and the accompanying materials are made
+available under the terms of the Eclipse Public License 2.0
+which is available at https://www.eclipse.org/legal/epl-2.0/
+
+SPDX-License-Identifier: EPL-2.0
+
+Contributors:
+    Smart City Jena
+-->
+<script setup lang="ts">
+import { OgcFeatureComposer } from "org.eclipse.daanse.board.app.lib.composer.ogc";
+import {
+  DatasourceRepository, identifier as DatasourceRepositoryIdentifier
+} from "org.eclipse.daanse.board.app.lib.repository.datasource";
+import { watch, ref, computed, onMounted } from "vue";
+import { container } from 'org.eclipse.daanse.board.app.lib.core'
+
+const { config, dataSources, connections } = defineProps<{
+  config: any;
+  dataSources: any;
+  connections: any;
+}>();
+
+const datasourcesFiltered = computed(() => {
+  return dataSources.filter((ds: any) => ds.type === 'csv');
+});
+
+const headers = ref([] as string[]);
+
+const geometryTypes = ['Point'];
+
+watch(() => config.connectedDatasources, async (newValue) => {
+  headers.value = await OgcFeatureComposer.getHeaders(
+    newValue,
+    container.get(DatasourceRepositoryIdentifier) as DatasourceRepository
+  );
+});
+
+watch(() => config.connectedDatasources, () => {
+    config.connectedDatasources = config.connectedDatasources.filter((ds: string) => ds !== null && ds !== undefined);
+})
+
+</script>
+<template>
+    <!-- eslint-disable-next-line vue/no-mutating-props -->
+  <VaSelect v-model="config.connectedDatasources" label="Sources" :options="datasourcesFiltered" multiple text-by="name"
+    value-by="uid" />
+
+  <!-- eslint-disable-next-line vue/no-mutating-props -->
+  <VaSelect v-model="config.xField" label="X Coordinate field" :options="headers" />
+
+  <!-- eslint-disable-next-line vue/no-mutating-props -->
+  <VaSelect v-model="config.yField" label="Y Coordinate field" :options="headers" />
+
+  <!-- eslint-disable-next-line vue/no-mutating-props -->
+  <VaSelect v-model="config.geometryType" label="Geometry type" :options="geometryTypes" />
+</template>
