4c9343ec879adaa7b8c60b03ab62c2945baf2ed2
vwfox <markus.hochstein@software-hochstein.de>
2026-04-28 16:11:22 +0200

    feat: auto-growing canvas with minimap in BaseLayout editor
    - Canvas auto-sizes based on widget positions with scrollbars
    - min-width/min-height always at least 100vw/100vh
    - Minimap preview shows widget positions and viewport rectangle
    - Click on minimap navigates to that area
    - Minimap only visible when content exceeds viewport
    - Page selector shifts right when minimap is visible (CSS :has)
    - Drag/drop coordinates account for scroll offset
    - Revert #794 flex layout back to absolute overlay

--- packages/app/default/src/pages/EditReport.vue
+++ packages/app/default/src/pages/EditReport.vue
@@ -70,35 +70,36 @@ const currentlyEditingWidget = computed(() => {
 </script>
 
 <template>
-  <div class="report-outer">
-    <div class="report-container dottet">
-      <LayoutRenderer
-        :pageId="pageID as string"
-        @openWidgetSettings="openWidgetSettings"
-      ></LayoutRenderer>
+  <div class="report-container dottet">
+    <LayoutRenderer
+      :pageId="pageID as string"
+      @openWidgetSettings="openWidgetSettings"
 
-      <div class="add_widget-button ice p-2.5 z-mx">
-        <VaButton
-          :icon="widgetSelectorVisible ? 'close' : 'add'"
-          @click="widgetSelectorVisible = !widgetSelectorVisible"
-          round
-          size="large"
-        />
-        <VaButton
-          v-if="endpointfinder_present"
-          icon="travel_explore"
-          @click="endPointFinder()"
-          round
-          size="large"
-        />
-      </div>
-      <div class="pages_board ice p-2.5 z-mx">
-        <PageEditor @pageSettings="(pageid)=>pageSettingsOpenedId = pageid"></PageEditor>
-      </div>
-      <Transition :duration="150">
-        <AddWidgetWindow v-if="widgetSelectorVisible"></AddWidgetWindow>
-      </Transition>
+    ></LayoutRenderer>
+
+    <div class="add_widget-button ice p-2.5 z-mx">
+
+      <VaButton
+        :icon="widgetSelectorVisible ? 'close' : 'add'"
+        @click="widgetSelectorVisible = !widgetSelectorVisible"
+        round
+        size="large"
+      />
+      <VaButton
+        v-if="endpointfinder_present"
+        icon="travel_explore"
+        @click="endPointFinder()"
+        round
+        size="large"
+      />
+
     </div>
+    <div class="pages_board ice p-2.5 z-mx">
+      <PageEditor @pageSettings="(pageid)=>pageSettingsOpenedId = pageid"></PageEditor>
+    </div>
+    <Transition :duration="150">
+      <AddWidgetWindow v-if="widgetSelectorVisible"></AddWidgetWindow>
+    </Transition>
     <Transition :duration="150">
       <WidgetSettingsWindow
         v-if="widgetSettingsOpenedId"
@@ -127,13 +128,6 @@ const currentlyEditingWidget = computed(() => {
 
 <style scoped>
 
-.report-outer {
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-  height: 100%;
-}
-
 .ghost-placeholder {
   position: absolute;
   background-color: rgba(0, 0, 0, 0.1);
@@ -148,8 +142,7 @@ const currentlyEditingWidget = computed(() => {
   justify-content: flex-start;
   align-items: flex-start;
   flex-direction: column;
-  flex: 1;
-  min-width: 0;
+  width: 100%;
   height: 100%;
   position: relative;
 }
