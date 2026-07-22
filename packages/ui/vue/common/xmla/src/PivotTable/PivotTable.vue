042f61eda1454f009a335cec81ad93d8e49eda3f
ssemenkoff <ssemenkoff@gmail.com>
2026-07-22 09:10:52 +0200

    Created drillthrough modal

--- packages/ui/vue/common/xmla/src/PivotTable/PivotTable.vue
+++ packages/ui/vue/common/xmla/src/PivotTable/PivotTable.vue
@@ -18,6 +18,7 @@ import { useElementSize } from "@vueuse/core";
 import RowsArea from "./Areas/RowsArea.vue";
 import ColumnsArea from "./Areas/ColumnsArea.vue";
 import CellsArea from "./Areas/CellsArea.vue";
+import DrillthroughModal from "./DrillthroughModal.vue";
 
 interface IPivotTable {
     rows: any[][];
@@ -171,7 +172,11 @@ const onStopResize = () => {
     eventBus.emit("onStopResize");
 };
 
-const drillthrough = () => { };
+const drillthroughModal = ref<InstanceType<typeof DrillthroughModal> | null>(null);
+
+const drillthrough = (cell: any) => {
+    drillthroughModal.value?.open(cell, data.value);
+};
 
 const columnsOffset = computed(() => {
     return data.value.rows?.[0]?.length * DEFAULT_COLUMN_WIDTH.value;
@@ -356,6 +361,14 @@ const totalContentSize = computed(() => {
                     :cellTextAlign="props.cellTextAlign"
                     :conditionalFormats="props.conditionalFormats"></CellsArea>
             </div>
+
+            <!-- Separate Drillthrough Modal Component -->
+            <DrillthroughModal
+                ref="drillthroughModal"
+                :cubeName="props.cubeName"
+                :propertiesRows="props.propertiesRows"
+                :propertiesCols="props.propertiesCols"
+            />
         </div>
     </template>
 </template>
